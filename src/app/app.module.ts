65ba5ba9d88b92fdb1303bd4d59fec68b9c06304
Icaro <iba1987@gmail.com>
2023-10-04 14:08:02 -0300

    build(icons): add ng-icons lib and package heroicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgIconsModule } from '@ng-icons/core';
+import * as heroIcons from '@ng-icons/heroicons/outline';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons(heroIcons),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
