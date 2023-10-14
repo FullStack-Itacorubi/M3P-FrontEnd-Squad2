cbed71bbd25623fbf4835ec64bfa051a810fe9d5
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-10-14 15:36:06 -0500

    feature(appointmentComponent): add validations and formatting

--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -9,24 +9,61 @@ import { Router } from '@angular/router';
 })
 export class AppointmentComponent {
   registerForm: FormGroup;
+  isEditMode: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private router: Router) {
     this.registerForm = this.formBuilder.group({
-      fullName: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]],
-      gender: ['', Validators.required],
-      cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
-      phoneNumber: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      userType: ['', Validators.required]
+      appointmentReason: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]],
+      appointmentDate: ['', Validators.required],
+      appointmentTime: ['', Validators.required],
+      problemDescription: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(1024)]],
+      prescribedMedication: [''],
+      dosageAndPrecautions: ['', [Validators.minLength(16), Validators.maxLength(256)]],
+      systemStatus: [true, Validators.required]
     });
   }
-  async onSubmit() {
+
+  getCurrentDate(): string {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    let mm: string | number = today.getMonth() + 1;
+    let dd: string | number = today.getDate();
+
+    if (mm < 10) mm = '0' + mm;
+    if (dd < 10) dd = '0' + dd;
+
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+  getCurrentTime(): string {
+    const now = new Date();
+    const hh: string | number = now.getHours();
+    const mm: string | number = now.getMinutes();
+
+    return `${hh}:${mm}`;
+  }
+
+  editForm() {
+    this.isEditMode = true;
+  }
+
+  deleteForm() {
+    // Implement logic for deleting the form
+    // You can show a confirmation dialog and proceed with deletion
+  }
+
+  onSubmit() {
+    // Implement form submission logic, including validation and saving the data
     if (this.registerForm.valid) {
-      alert('Cadastro efetuado com sucesso!');
+      // Generate a unique identifier for this consultation
+      const uniqueIdentifier = 'your-unique-identifier'; // Implement your own logic here
+
+      alert(`Consulta registrada com sucesso! Identificador único: ${uniqueIdentifier}`);
       this.router.navigate(['login']);
     } else {
       alert('Existem dados inválidos no formulário. Por favor, corrija-os e tente novamente.');
     }
+
+    this.isEditMode = false;
   }
 }
