982ab5529992d634b8d5e077b571d5e29da99bb4
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-10-13 19:00:54 -0500

    feature(AppointmentComponent): create appointment registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomepageComponent } from './components/homepage/homepage.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { RegistryNotAlowedComponent } from './components/registry-not-alowed/registry-not-alowed.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -31,6 +32,11 @@ const routes: Routes = [
       {
         path: 'register',
         component: SignupComponent
+        // canActivate: [AuthGuard]
+      },
+      {
+        path: 'appointment',
+        component: AppointmentComponent
         // canActivate: [AuthGuard]
       },
     ]
--- src/app/components/appointment/appointment.component.ts
+++ src/app/components/appointment/appointment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-appointment',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./appointment.component.scss']
 })
 export class AppointmentComponent {
+  registerForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]],
+      gender: ['', Validators.required],
+      cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
+      phoneNumber: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      userType: ['', Validators.required]
+    });
+  }
+  async onSubmit() {
+    if (this.registerForm.valid) {
+      alert('Cadastro efetuado com sucesso!');
+      this.router.navigate(['login']);
+    } else {
+      alert('Existem dados inválidos no formulário. Por favor, corrija-os e tente novamente.');
+    }
+  }
 }
